6855354f97d0a8d055c70187255d8b671fa16a80
mohayax <mohayax02@gmail.com>
2023-01-18 11:36:01 +0100

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import NavigationBar from "./components/NavigationBar";
-import { FaBeer } from 'react-icons/fa';
+
 
 function App() {
   return (
    <div>
     <NavigationBar />
-      <FaBeer style={{color: 'red'}}/>
+      
    </div>
   )
 
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,30 +1,47 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import './NavigationBar.css'
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {FaGithub} from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 
 function NavigationBar  () {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container className='container-fluid nav_margin'>
+        <Navbar.Toggle aria-controls="navbarScroll"/>
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <Nav.Link href="#">
-              Link
+
+            <Nav.Link href="mailto:mohayax02@gmail.com">
+              <FaEnvelope className='icon' />
+            </Nav.Link>
+
+            <Nav.Link href="https://github.com/mohayax" target="blank">
+              <FaGithub className='icon'/>
+            </Nav.Link>
+           
+            <Nav.Link href="#action2">
+              <FaLinkedin className='icon'/>
+            </Nav.Link>
+          
+            <Nav.Link href="#action2">
+              <FaTwitter className='icon' />
             </Nav.Link>
           </Nav>
-        
+        <div className="d-flex">
+          <Nav.Link href="#projects" style={{color:"white"}} className="me-4">Projects</Nav.Link>
+          
+          <Nav.Link href="#contact" style={{ color: "white" }} className="me-4">Contact Me</Nav.Link>
+        </div>
             
         </Navbar.Collapse>
       </Container>
